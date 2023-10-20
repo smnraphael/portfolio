cc672e8ee61698c8c9a93a526c1d8d76c637ecef
Raphaël <simonraphael@hotmail.fr>
2023-10-20 12:01:30 +0200

    copywriting

--- src/components/About.js
+++ src/components/About.js
@@ -16,6 +16,9 @@ function About() {
         <p className="about-text">
           After various certifications like Google Data Analytics Professional Certificate (Google), CS50x: Introduction to Computer Science (Harvard University), and currently taking Full-Stack Engineer Carrer Path (Codecademy), I developed a knowledge of HTML, CSS, Bootstrap, JavaScript, React, Python, Flask, and SQL.
         </p>
+        <p className="about-text">
+          Proficient in three languages, including French (native), English (fluent), and Japanese (JLPT N4 level), I combine technical expertise with strong problem-solving abilities and effective cross-cultural communication, enabling me to drive quality and efficiency while understanding diverse markets and cultures.
+        </p>
     </div>
   );
 }
