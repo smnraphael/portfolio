ce53ccfebd2f6e6f07f3b07b79249b259fd073dc
smnraphael <simonraphael@hotmail.fr>
2024-04-25 21:26:05 +0200

    fix dark mode for certification card

--- src/components/CertificationCard.jsx
+++ src/components/CertificationCard.jsx
@@ -1,17 +1,19 @@
 function CertificationCard({ course, school, date, link }) {
   return (
     <div className="max-w-sm p-6 bg-white border border-stone-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
-      <p className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      <p className="text-2xl font-bold mb-1 text-gray-900 dark:text-white">
         {course}
       </p>
-      <p>{school}</p>
-      <a
-        href={link}
-        target="_blank"
-        className="text-sm text-blue-500 hover:underline"
-      >
-        View certifcate
-      </a>
+      <p className="dark:text-white mb-2">{school}</p>
+      <div className="mb-2">
+        <a
+          href={link}
+          target="_blank"
+          className="text-sm text-blue-500 hover:underline"
+        >
+          View certifcate
+        </a>
+      </div>
       <p className="block text-xs text-gray-400 dark:text-gray-500">{date}</p>
     </div>
   );
